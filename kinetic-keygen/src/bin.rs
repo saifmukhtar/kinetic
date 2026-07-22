0d88dfee0ee46c17d94b202d288196c1db615d5f
saifmukhtar <saifmukhtar20@gmail.com>
2026-07-22 22:04:59 +0530

    audited kinetic-keygen src/bin.rs

--- kinetic-keygen/src/bin.rs
+++ kinetic-keygen/src/bin.rs
@@ -1,5 +1,6 @@
-use ml_dsa::{MlDsa65, KeyExport};
-use ml_dsa::signature::Keypair;
+//! Auxiliary binary utility for computing static ML-DSA-65 council public keys from seeds.
+
+use ml_dsa::{KeyExport, MlDsa65};
 
 fn main() {
     let bytes1 = hex::decode("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855").unwrap();
