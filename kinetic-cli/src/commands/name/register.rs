0e8f9987d66a3bc3970ddd65b67b79d037ffdbb7
saifmukhtar <saifmukhtar20@gmail.com>
2026-08-01 23:36:04 +0530

    chore: audit and update register.rs

--- kinetic-cli/src/commands/name/register.rs
+++ kinetic-cli/src/commands/name/register.rs
@@ -323,7 +323,7 @@ pub async fn handle(
             );
             let _ = save_zone_file(&fqdn, &zone);
             let reveal_path = get_zones_dir().join(format!("{}.reveal.json", fqdn));
-            let record = kinetic_core::types::DomainRecord::Standard(reveal);
+            let record = kinetic_core::types::DomainRecord::Standard(Box::new(reveal));
             let reveal_str =
                 serde_json::to_string_pretty(&record).expect("Failed to serialize Reveal");
             let _ = std::fs::write(&reveal_path, reveal_str);
