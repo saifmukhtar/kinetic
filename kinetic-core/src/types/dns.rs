3e55ac5b511fd51f39ffcbe64d8a37cacd4e19a3
saifmukhtar <saifmukhtar20@gmail.com>
2026-08-18 18:07:52 +0530

    refactor(core): remove raw tracing string from dns validation

--- kinetic-core/src/types/dns.rs
+++ kinetic-core/src/types/dns.rs
@@ -114,7 +114,6 @@ impl DnsZoneExt for DnsZone {
                     }
                     DnsRecord::KID(kid_str) => {
                         if !kid_str.starts_with(crate::constants::DID_PREFIX) {
-                            tracing::warn!("Rejecting non-Kinetic DID CNAME: {}", kid_str);
                             return Err(crate::error::DnsError::InvalidKid(kid_str.clone()));
                         }
                     }
